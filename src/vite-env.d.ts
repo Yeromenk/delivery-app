44df600c64f9557e6463806f2133a0907af213d6
Maksym Yeromenko <maksimeromenko@gmail.com>
2025-09-18 16:11:00 +0200

    Add TypeScript configuration for Vite; include vite-env.d.ts for module declarations

---
+++ src/vite-env.d.ts
@@ -0,0 +1,50 @@
+/// <reference types="vite/client" />
+
+declare module '*.css' {
+    const content: { [className: string]: string };
+    export default content;
+}
+
+declare module '*.scss' {
+    const content: { [className: string]: string };
+    export default content;
+}
+
+declare module '*.png' {
+    const src: string;
+    export default src;
+}
+
+declare module '*.jpg' {
+    const src: string;
+    export default src;
+}
+
+declare module '*.jpeg' {
+    const src: string;
+    export default src;
+}
+
+declare module '*.gif' {
+    const src: string;
+    export default src;
+}
+
+declare module '*.svg' {
+    const src: string;
+    export default src;
+}
+
+declare module '*.webp' {
+    const src: string;
+    export default src;
+}
+
+interface ImportMetaEnv {
+    readonly VITE_API_URL?: string;
+    // more env variables...
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
